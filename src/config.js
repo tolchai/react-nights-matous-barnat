05b4126773479adf50c577994e58faff2b28f6ee
Matouš Barnat <matous.barnat@gmail.com>
2019-03-20 11:30:43 +0100

    init

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  clientId: '9fd02a643d129f1617f8f149f2642181f184e48873915f8159496eda7f568cbf',
+  scope: 'market:452',
+  apiUrl: 'https://the-purple-brand-24.commercelayer.io',
+}
