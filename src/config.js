cb7cdbb3d803c5a2fd31871f7892a1096a20c8e5
Matouš Barnat <matous.barnat@gmail.com>
2019-03-22 16:43:08 +0100

    env secret

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
   clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
   scope: process.env.REACT_APP_SCOPE,
   apiUrl: process.env.REACT_APP_API_URL,
 }
