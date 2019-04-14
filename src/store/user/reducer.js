004f55b4eb908ca804eeeab1e9dd71232cbfbd58
Matouš Barnat <matous.barnat@gmail.com>
2019-04-14 18:52:37 +0200

    login form, fetching customer token

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -3,12 +3,14 @@ import { LOGIN_USER, LOGOUT_USER } from './actions'
 const reducer = (state = {}, action) => {
   switch (action.type) {
     case LOGIN_USER:
-      console.log(action.payload)
       return {
         ...state,
         ...action.payload,
       }
     case LOGOUT_USER: {
+      // const nextState = Object.assign({}, state)
+      // delete nextState['user']
+      // return nextState
       return state
     }
     default:
